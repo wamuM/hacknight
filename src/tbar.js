10552088845c753ff64de49a8abd1229479ab0cc
pauulemon <paulaquetineau@hotmail.com>
2023-09-30 01:22:43 +0200

    pipipi

---
+++ src/tbar.js
@@ -0,0 +1,9 @@
+var fn;
+function setbar(progress, total)
+
+
+function updatebar(step, total)
+    var element = document.getElementById("tbar");
+    var width = 1;
+    var identity = setInterval(scene, 10);
+    function
